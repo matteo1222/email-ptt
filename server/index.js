abc658c9a34c6e0a55c51acb86cc2d96a6562c8e
matteo1222 <49985851+matteo1222@users.noreply.github.com>
2020-07-03 01:55:29 +0800

    testing ptt webscrap

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
+
+/*
 const express = require('express');
 const {spawn} = require('child_process');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.get('/', (req, res) => {
     let dataToSend;
 
-    const python = spawn('python', ['script1.py']);
+    const python = spawn('python', ['./script1.py']);
 
     python.stdout.on('data', function(data){
         console.log('Pipe data from python script...');
@@ -20,3 +22,32 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
+*/
+
+const express = require('express')
+const app = express()
+let { PythonShell } = require('python-shell')
+
+app.listen(3000, () => {
+  console.log('server running on port 3000')
+})
+
+app.get('/call/python', pythonProcess)
+
+function pythonProcess(req, res) {
+  let options = {
+    args:
+      [
+        req.query.name,
+        req.query.from
+      ]
+  }
+
+  PythonShell.run('./script1.py', options, (err, data) => {
+    if (err) res.send(err)
+    const parsedString = JSON.parse(data)
+    console.log(`name: ${parsedString.Name}, from: ${parsedString.From}`)
+    res.json(parsedString)
+  })
+
+}
--- server/script1.py
+++ server/script1.py
@@ -1,1 +1,12 @@
-print('Hello from python')
+import sys 
+import json
+
+result = {
+    'Name': sys.argv[1],
+    'From': sys.argv[2]
+  }
+
+json = json.dumps(result)
+
+print(str(json))
+sys.stdout.flush()
